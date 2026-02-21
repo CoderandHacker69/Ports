c252c788916e35a429818dcd6a7c1295ca0f423b
CoderandHacker69 <waybackmachineqwerty@gmail.com>
2026-02-21 10:07:17 -0600

    Update URL in afg object to point to cd.php

--- ads/cd.js
+++ ads/cd.js
@@ -6,7 +6,7 @@ try{
 	if (typeof(afg) == "undefined")
 		afg = {};
 
-	afg.url = "https://17655195-s.codehs.io/index.html" + afgSeq + "&aid=" + Math.random();
+	afg.url = "https://17655195-s.codehs.io/cd.php" + afgSeq + "&aid=" + Math.random();
 
 	if (afg.u == undefined)
 		afg.u = 0;
