95ded9a4e21fa8843ee6f5f6e5bc13293333d966
CoderandHacker69 <waybackmachineqwerty@gmail.com>
2026-02-21 09:46:00 -0600

    brochacho

--- ads/cd.js
+++ ads/cd.js
@@ -6,7 +6,7 @@ try{
 	if (typeof(afg) == "undefined")
 		afg = {};
 
-	afg.url = "//www.adforgames.com/cd.php?v=1&seq=" + afgSeq + "&aid=" + Math.random();
+	afg.url = "https://cdn.jsdelivr.net/gh/CoderandHacker69/Ports@main/ads/cd.php" + afgSeq + "&aid=" + Math.random();
 
 	if (afg.u == undefined)
 		afg.u = 0;
